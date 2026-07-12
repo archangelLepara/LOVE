82e26ad0563bb20df68ebebc67fefb0bff01d385
archangelLepara <archangellepara@gmail.com>
2026-07-12 19:07:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const pages = document.querySelectorAll(".page");
+    const nextButtons = document.querySelectorAll(".next-btn");
+    const progressBar = document.getElementById("progressBar");
+
+    let currentPage = 0;
+
+
+    function updateProgress(){
+
+        let progress = (currentPage / (pages.length - 1)) * 100;
+        progressBar.style.width = progress + "%";
+
+    }
+
+
+    nextButtons.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+
+            pages[currentPage].classList.remove("active");
+
+
+            currentPage++;
+
+
+            if(currentPage < pages.length){
+
+                pages[currentPage].classList.add("active");
+
+            }
+
+
+            updateProgress();
+
+
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+
+
+        });
+
+    });
+
+
+
+    document.getElementById("submitBtn").addEventListener("click", function(){
+
+
+        const message = "Test message - Relationship Reset ❤️";
+
+
+        const phoneNumber = "YOUR_NUMBER_HERE";
+
+
+        const whatsappURL =
+        "https://wa.me/" +
+        phoneNumber +
+        "?text=" +
+        encodeURIComponent(message);
+
+
+
+        window.location.href = whatsappURL;
+
+
+    });
+
+
+    updateProgress();
+
+});
